07b6a7edefda647176f864ccd4282f82faecacb6
lamhuynhb779 <lamhuynhb779@gmail.com>
2023-04-08 21:04:39 +0700

    rabbitmq-guide update rabbit port

--- config/provider-connection.php
+++ config/provider-connection.php
@@ -8,7 +8,7 @@ return [
         'hosts' => [
             [
                 'host' => env('RABBITMQ_HOST', '127.0.0.1'),
-                'port' => env('RABBITMQ_PORT', 15672),
+                'port' => env('RABBITMQ_PORT', 5672),
                 'user' => env('RABBITMQ_USER', 'guest'),
                 'password' => env('RABBITMQ_PASSWORD', 'guest'),
                 'vhost' => env('RABBITMQ_VHOST', '/'),
